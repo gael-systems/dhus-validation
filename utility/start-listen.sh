194c95e251ab88db718d49679136b58b23067f78
Thibaud Degueurce <tdegueurce@gael.fr>
2016-11-15 17:18:23 +0100

    reverting start-listen.sh changes

--- utility/start-listen.sh
+++ utility/start-listen.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 cd /home/dhus/go-dhus-environment/dhus \
-&& ./start.sh \
+&& ./start.sh > /dev/null 2>&1 \
 & java -jar /home/dhus/go-dhus-environment/dhus-listener.jar 180 /home/dhus/go-dhus-environment/dhus/dhus.log
