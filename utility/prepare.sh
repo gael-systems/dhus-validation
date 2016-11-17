c728e89366d705e47a714b49b956a390306a8f61
Thibaud Degueurce <tdegueurce@gael.fr>
2016-11-17 16:48:17 +0100

    added reference database copy in prepare.sh

--- utility/prepare.sh
+++ utility/prepare.sh
@@ -5,6 +5,10 @@ cd /home/dhus/go-dhus-environment \
 && unzip -o dhus-software-distribution.zip -d ./dhus \
 && rm dhus-software-distribution.zip \
 && sed -ie "s|local_dhus|/home/dhus/go-dhus-environment/dhus/local_dhus|" dhus/etc/dhus.xml \
+&& mkdir ./dhus/local_dhus \
+&& cp -r /data/ivv/dhus-test-backup/database ./dhus/local_dhus \
+&& mkdir ./dhus/local_dhus/solr/dhus/data \
+&& cp -r /data/ivv/dhus-test-backup/index ./dhus/local_dhus/solr/dhus/data \
 && cp start.sh ./dhus/start.sh \
 && chmod +x ./dhus/start.sh \
 && chmod +x ./dhus/stop.sh \
