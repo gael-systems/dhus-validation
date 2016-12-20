bb62e48aacaf46e7c80cc6a760958bc9a95176e4
Thibaud Degueurce <thibaud.degueurce@gael.fr>
2016-12-20 14:15:03 +0100

    Remove custom dhus.xml, improve prepare.sh

--- utility/prepare.sh
+++ utility/prepare.sh
@@ -4,13 +4,10 @@ cd /home/dhus/go-dhus-environment \
 && mkdir ./dhus \
 && unzip -o dhus-software-distribution.zip -d ./dhus \
 && rm dhus-software-distribution.zip \
-&& mkdir ./dhus/local_dhus \
-&& cp -r /data/ivv/dhus-test-backup/database ./dhus/local_dhus \
+&& sed -ie "s|local_dhus|/home/dhus/go-dhus-environment/dhus/local_dhus|" dhus/etc/dhus.xml \
+&& cp -r /data/ivv/dhus-test-backup/local_dhus ./dhus \
 && mkdir -p ./dhus/local_dhus/solr/dhus/data \
 && cp -r /data/ivv/dhus-test-backup/index ./dhus/local_dhus/solr/dhus/data \
 && cp -r /data/ivv/dhus-test-backup/incoming ./dhus/local_dhus \
 && cp start.sh ./dhus/start.sh \
-&& chmod +x ./dhus/start.sh \
-&& chmod +x ./dhus/stop.sh \
-&& chmod +x ./start-listen.sh \
-&& cp dhus.xml ./dhus/etc/dhus.xml
+&& chmod +x ./dhus/start.sh ./dhus/stop.sh ./start-listen.sh
