71b0b764ffb87fe33a80b1f6fed490a93bb648b1
Thibaud Degueurce <thibaud.degueurce@gael.fr>
2016-12-22 12:00:46 +0100

    Remove deletion of distrib zip in prepare.sh

--- utility/prepare.sh
+++ utility/prepare.sh
@@ -3,7 +3,6 @@ cd /home/dhus/go-dhus-environment \
 && rm -rf ./dhus \
 && mkdir ./dhus \
 && unzip -o dhus-software-distribution.zip -d ./dhus \
-&& rm dhus-software-distribution.zip \
 && sed -ie "s|local_dhus|/home/dhus/go-dhus-environment/dhus/local_dhus|" dhus/etc/dhus.xml \
 && cp -r /data/ivv/dhus-test-backup/local_dhus ./dhus \
 && cp start.sh ./dhus/start.sh \
