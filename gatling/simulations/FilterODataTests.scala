4b586a90dfcdb194f1e374ea4369819ccd2c33f1
Thibaud Degueurce <thibaud.degueurce@gael.fr>
2017-02-13 14:24:39 +0100

    Add orderby in FilterODataTests.scala

--- gatling/simulations/FilterODataTests.scala
+++ gatling/simulations/FilterODataTests.scala
@@ -17,9 +17,6 @@ class FilterODataTests extends Simulation {
 	    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 	    .basicAuth("root", "rootpassword") // create user root on centos1 dhus
 
-	val sets = csv("entitysets.csv").queue // fields: entitySetName
-	val operators = csv("operators.csv").queue // fields: operator
-
 	/* 
 	 TODO 
 	 	- add feeders that provide filter arguments
@@ -32,48 +29,54 @@ class FilterODataTests extends Simulation {
 				.get("/").check(status.is(200)))
 			.pause(1)
 			.feed(properties)
-			.exec(http(entitySetName+" ${property} eq")
+			.exec(http("Filter "+entitySetName+" ${property} Eq")
 				.get("/"+entitySetName+"?$filter=${property} eq ${eqValue}").check(status.is(200)))
 			.pause(1)
-			.exec(http(entitySetName+" ${property} ne")
+			.exec(http("Filter "+entitySetName+" ${property} Ne")
 				.get("/"+entitySetName+"?$filter=${property} ne ${neValue}").check(status.is(200)))
-		} 
+			.pause(1)
+			.exec(http("OrderBy "+entitySetName+" ${property} Asc")
+				.get("/"+entitySetName+"?$orderby=${property} asc").check(status.is(200)))
+			.pause(1)
+			.exec(http("OrderBy "+entitySetName+" ${property} Desc")
+				.get("/"+entitySetName+"?$orderby=${property} desc").check(status.is(200)))
+			.pause(1)
+		}
 	}
 
 	// fields in properties feeders: property, eqValue, neValue
-	object Connections extends EntitySet("Connections", csv("custom-Connections-properties.csv").queue, 9)
-	object Networks extends EntitySet("Networks", csv("custom-Networks-properties.csv").queue, 1)
-	object Products extends EntitySet("Products", csv("custom-Products-properties.csv").queue, 8)
-	object Synchronizers extends EntitySet("Synchronizers", csv("custom-Synchronizers-properties.csv").queue, 16)
-	object Users extends EntitySet("Users", csv("custom-Users-properties.csv").queue, 12)
-	object Ingests extends EntitySet("Ingests", csv("custom-Ingests-properties.csv").queue, 6)
-	object UserSynchronizers extends EntitySet("UserSynchronizers", csv("custom-UserSynchronizers-properties.csv").queue, 15)
-	object Collections extends EntitySet("Collections", csv("custom-Collections-properties.csv").queue, 2)
-	object Classes extends EntitySet("Classes", csv("custom-Classes-properties.csv").queue, 2)
-	object DeletedProducts extends EntitySet("DeletedProducts", csv("custom-DeletedProducts-properties.csv").queue, 10)
+	object Connections extends 			EntitySet("Connections", 		csv("custom-Connections-properties.csv").circular, 9)
+	object Networks extends 			EntitySet("Networks", 			csv("custom-Networks-properties.csv").circular, 1)
+	object Products extends 			EntitySet("Products", 			csv("custom-Products-properties.csv").circular, 8)
+	object Synchronizers extends 		EntitySet("Synchronizers", 		csv("custom-Synchronizers-properties.csv").circular, 16)
+	object Users extends 				EntitySet("Users", 				csv("custom-Users-properties.csv").circular, 12)
+	object Ingests extends 				EntitySet("Ingests", 			csv("custom-Ingests-properties.csv").circular, 6)
+	object UserSynchronizers extends 	EntitySet("UserSynchronizers", 	csv("custom-UserSynchronizers-properties.csv").circular, 15)
+	object Collections extends 			EntitySet("Collections", 		csv("custom-Collections-properties.csv").circular, 2)
+	object Classes extends 				EntitySet("Classes", 			csv("custom-Classes-properties.csv").circular, 2)
+	object DeletedProducts extends 		EntitySet("DeletedProducts", 	csv("custom-DeletedProducts-properties.csv").circular, 10)
 
-	val filterConnections = scenario("Filter Connections").exec(Connections.filter)
-	val filterNetworks = scenario("Filter Networks").exec(Networks.filter)
-	val filterProducts = scenario("Filter Products").exec(Products.filter)
-	val filterSynchronizers = scenario("Filter Synchronizers").exec(Synchronizers.filter)
-	val filterUsers = scenario("Filter Users").exec(Users.filter)
-	val filterIngests = scenario("Filter Ingests").exec(Ingests.filter)
-	val filterUserSynchronizers = scenario("Filter UserSynchronizers").exec(UserSynchronizers.filter)
-	val filterCollections = scenario("Filter Collections").exec(Collections.filter)
-	val filterClasses = scenario("Filter Classes").exec(Classes.filter)
-	val filterDeletedProducts = scenario("Filter DeletedProducts").exec(DeletedProducts.filter)
-	
+	val filterConnections = 		scenario("Connections").exec(Connections.filter)
+	val filterNetworks = 			scenario("Networks").exec(Networks.filter)
+	val filterProducts = 			scenario("Products").exec(Products.filter)
+	val filterSynchronizers = 		scenario("Synchronizers").exec(Synchronizers.filter)
+	val filterUsers = 				scenario("Users").exec(Users.filter)
+	val filterIngests = 			scenario("Ingests").exec(Ingests.filter)
+	val filterUserSynchronizers = 	scenario("UserSynchronizers").exec(UserSynchronizers.filter)
+	val filterCollections = 		scenario("Collections").exec(Collections.filter)
+	val filterClasses = 			scenario("Classes").exec(Classes.filter)
+	val filterDeletedProducts = 	scenario("DeletedProducts").exec(DeletedProducts.filter)
+
 	setUp(
-    	filterConnections.inject(rampUsers(1) over (20 seconds)), // scenario will be executed i times over n seconds
-    	filterNetworks.inject(rampUsers(1) over (20 seconds)),
-    	filterProducts.inject(rampUsers(1) over (20 seconds)),
-    	filterSynchronizers.inject(rampUsers(1) over (20 seconds)),
-    	filterUsers.inject(rampUsers(1) over (20 seconds)),
-    	filterIngests.inject(rampUsers(1) over (20 seconds)),
-    	filterUserSynchronizers.inject(rampUsers(1) over (20 seconds)),
-    	filterCollections.inject(rampUsers(1) over (20 seconds)),
-    	filterClasses.inject(rampUsers(1) over (20 seconds)),
-    	filterDeletedProducts.inject(rampUsers(1) over (20 seconds))
-  	).assertions(global.failedRequests.percent.is(0))
+    	filterConnections.inject(		nothingFor( 0 seconds), rampUsers(1) over (20 seconds)), // scenario will be executed i times over n seconds
+    	filterNetworks.inject(			nothingFor( 5 seconds), rampUsers(1) over (20 seconds)),
+    	filterProducts.inject(			nothingFor(10 seconds), rampUsers(1) over (20 seconds)),
+    	filterSynchronizers.inject(		nothingFor(15 seconds), rampUsers(1) over (20 seconds)),
+    	filterUsers.inject(				nothingFor(20 seconds), rampUsers(1) over (20 seconds)),
+    	filterIngests.inject(			nothingFor(25 seconds), rampUsers(1) over (20 seconds)),
+    	filterUserSynchronizers.inject(	nothingFor(30 seconds), rampUsers(1) over (20 seconds)),
+    	filterCollections.inject(		nothingFor(35 seconds), rampUsers(1) over (20 seconds)),
+    	filterClasses.inject(			nothingFor(40 seconds), rampUsers(1) over (20 seconds)),
+    	filterDeletedProducts.inject(	nothingFor(45 seconds), rampUsers(1) over (20 seconds)))
   	.protocols(httpConf)
 }
