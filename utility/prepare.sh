9afa070ae9dded62678eacf33cfde112d7c07134
Thibaud Degueurce <thibaud.degueurce@gael.fr>
2017-02-17 09:35:51 +0100

    Remove backup dhus.xml in prepare.sh

--- utility/prepare.sh
+++ utility/prepare.sh
@@ -3,7 +3,7 @@ cd /home/dhus/go-dhus-environment \
 && rm -rf ./dhus \
 && mkdir ./dhus \
 && unzip -o dhus-software-distribution.zip -d ./dhus \
-&& sed -ie "s|local_dhus|/home/dhus/go-dhus-environment/dhus/local_dhus|" dhus/etc/dhus.xml \
+&& sed -i "s|local_dhus|/home/dhus/go-dhus-environment/dhus/local_dhus|" dhus/etc/dhus.xml \
 && cp -r /data/ivv/dhus-test-backup/local_dhus ./dhus \
 && cp start.sh ./dhus/start.sh \
 && chmod +x ./dhus/start.sh ./dhus/stop.sh ./start-listen.sh
